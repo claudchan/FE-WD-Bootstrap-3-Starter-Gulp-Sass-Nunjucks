b518161a54d16d7c27a7e8075aa4a0ffa128ca7a
Chan Chee Chiew <cheechiew.chan@mirumagency.com>
2017-10-24 10:12:31 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@
 */
 
 var gulp = require('gulp'),
+  watch = require('gulp-watch'),
   config = require('./config.json'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
@@ -86,8 +87,9 @@ gulp.task('styles', function () {
 });
 
 // Nunjucks tasks
-gulp.task('nunjucks', function () {
-  return gulp.src(config.path.src + '/templates/pages/**/*.+(html|nunjucks)')
+// task to render html
+gulp.task('nunjucks:render', function () {
+  return watch(config.path.src + '/templates/pages/**/*.+(html|nunjucks)', { ignoreInitial: false })
     // Renders template with nunjucks
     .pipe(
       nunjucksRender({
@@ -95,7 +97,14 @@ gulp.task('nunjucks', function () {
       })
     )
     // Output files in app folder
-    .pipe(gulp.dest(config.path.src));
+    .pipe(gulp.dest(config.path.src))
+    .pipe(reload({stream: true}));
+});
+
+// task to reload after nunjuck's rendered
+gulp.task('nunjucks', ['nunjucks:render'], function (done) {
+  browserSync.reload();
+  done();
 });
 
 // Browser-Sync tasks
@@ -163,5 +172,5 @@ gulp.task ('watch', function () {
 });
 
 // Gulp default
-gulp.task('default', ['nunjucks', 'scripts', 'styles', 'browser-sync', 'watch']);
+gulp.task('default', ['scripts', 'styles', 'nunjucks', 'browser-sync', 'watch']);
 
